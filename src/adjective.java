12863ac2f1958d859ad4e49a910fb50bb33eca5c
Ian Worley <therealianw@gmail.com>
2020-11-02 09:30:08 -0600

    Working on adding docs to the code.

--- src/adjective.java
+++ src/adjective.java
@@ -1,7 +1,14 @@
 public class adjective extends noun {
+    /**
+     * @param phrase creates a new adjective with a given string.
+     */
     public adjective(String phrase) {
         super(phrase);
     }
+
+    /**
+     *
+     */
     public adjective() {
         super();
     }
